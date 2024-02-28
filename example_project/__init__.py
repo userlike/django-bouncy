538649c6f28a446676a2cf0668cad198b2fff50e
Marc Migge <marc.migge@userlike.com>
2024-02-28 13:52:56 +0100

    Clean up dependencies and get tests running again

--- example_project/__init__.py
+++ example_project/__init__.py
@@ -1,42 +1,51 @@
 # Based off microdjango by J. Cliff Dyer
 # Get it at https://bitbucket.org/cliff/microdjango/
 
-ROOT_URLCONF = 'example_project.urls'
-SECRET_KEY = u'None'
+ROOT_URLCONF = "example_project.urls"
+SECRET_KEY = "None"
 DEBUG = True
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 SITE_ID = 1
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': './example.db',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "./example.db",
     }
 }
 
-BOUNCY_TOPIC_ARN = ['arn:aws:sns:us-east-1:250214102493:Demo_App_Unsubscribes']
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+BOUNCY_TOPIC_ARN = ["arn:aws:sns:us-east-1:250214102493:Demo_App_Unsubscribes"]
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django_bouncy'
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django_bouncy",
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+MIDDLEWARE = (
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
 )
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-NOSE_ARGS = [
-    '--with-xunit',
-    '--nologcapture',
-    '--cover-package=django_bouncy',
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
+            ],
+        },
+    }
 ]
